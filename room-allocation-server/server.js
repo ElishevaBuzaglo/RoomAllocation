c14fde5b23d17f6a081b453358d67ca050c4c4ce
shbh8205-lgtm <shbh8205@gmail.com>
2026-04-23 00:13:08 +0300

    Update server.js

--- room-allocation-server/server.js
+++ room-allocation-server/server.js
@@ -1,75 +1,25 @@
-<<<<<<< Shoshana
-// בס"ד
-// במקום require
-import dns from 'node:dns';
-dns.setServers(['8.8.8.8', '1.1.1.1']);
-
-import 'dotenv/config'; //.env טוען את המשתנים מקובץ ה-
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-=======
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import roomRoutes from "./api/routes/roomRoutes.js"; // חובה לציין סיומת .js
 import allocationRoutes from "./api/routes/allocationRoutes.js"; // חובה לציין סיומת .js
->>>>>>> main
 const app = express();
 
-// Middleware לקריאת JSON
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< Shoshana
-// הגדרת משתנים מה-env
-=======
 // ניתוב הבקשות
 app.use("/api/rooms", roomRoutes);
 //ניתוב לשיבוצים
 app.use("/api/allocations", allocationRoutes);
 
->>>>>>> main
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI;
-// const mongoURI = 'mongodb+srv://<username>:<password>@cluster.mongodb.net/SeminarDB';
 
-const RoomSchema = new mongoose.Schema({
-  wing: String,
-  floor: Number,
-  size: Number,
-  hasProjector: Boolean,
-  status: String,
-});
-
-const Room = mongoose.model("Room", RoomSchema, "Rooms"); 
-// נתיב לקבלת רשימת החדרים
-app.get("/api/rooms", async (req, res) => {
-  try {
-    const rooms = await Room.find();    
-    res.json(rooms);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// נתיב בדיקה ראשוני
-app.get("/", (req, res) => {
-  res.send("Room Allocation API is running...");
-});
- // חיבור למסד הנתונים
-mongoose
-  .connect(mongoURI)
+mongoose.connect(mongoURI)
   .then(() => {
-    console.log("Connected to MongoDB successfully!");
-    // רק אחרי שהחיבור הצליח, השרת מתחיל להאזין
-    app.listen(PORT, () => {
-      console.log(`🚀Server is running on http://localhost:${PORT}`);
-    });
+    console.log("✅ Connected to MongoDB successfully!");
+    app.listen(PORT, () => console.log(`🚀 Server on http://localhost:${PORT}`));
   })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1); // סגירת האפליקציה במקרה של שגיאת חיבור קריטית
-  });
+  .catch(err => console.error("❌ MongoDB Error:", err.message));
