8b8a466d557b7206ebe015159cd8c780524a6a48
Elisheva Buzaglo <elishevab002@gmail.com>
2026-04-22 14:34:30 +0300

    Resolved the conflict.

--- room-allocation-server/api/routes/roomRoutes.js
+++ room-allocation-server/api/routes/roomRoutes.js
@@ -21,26 +21,3 @@ router.route("/:id")
   .delete(deleteRoom);
 
 export default router;
-// import express from "express";
-// import { 
-//     createRoom, 
-//     getAllRooms, 
-//     getRoomById, 
-//     updateRoom, 
-//     deleteRoom 
-// } from "../controllers/roomController.js";
-
-// const router = express.Router();
-
-// // נתיב ליצירת חדר וקבלת כל החדרים
-// router.route("/")
-//     .post(createRoom)    // POST /api/rooms
-//     .get(getAllRooms);   // GET /api/rooms
-
-// // נתיבים לחדר ספציפי לפי ID
-// router.route("/:id")
-//     .get(getRoomById)    // GET /api/rooms/12345 (כולל ה-Populate)
-//     .put(updateRoom)    // PUT /api/rooms/12345
-//     .delete(deleteRoom); // DELETE /api/rooms/12345
-
-// export default router;
