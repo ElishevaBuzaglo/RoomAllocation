1aaa1c57a47a07532764c08728be34b13003cf45
Shoshana Ben Haim <shbh8205@gmail.com>
2026-04-29 02:30:20 +0300

    fix: update room routes

--- room-allocation-server/api/routes/roomRoutes.js
+++ room-allocation-server/api/routes/roomRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { 
-  createRoom, 
-  getAllRooms, 
-  getRoomById, 
-  updateRoom, 
+import {
+  createRoom,
+  getAllRooms,
+  getRoomById,
+  updateRoom,
   deleteRoom,
   searchRooms
 } from "../controllers/roomController.js";
@@ -12,15 +12,16 @@ import { validateRoomSearch } from '../middlewares/roomValidation.js';
 const router = express.Router();
 
 router.route("/")
-    .get(getAllRooms)
-    .post(createRoom);
+  .get(getAllRooms)
+  .post(createRoom);
+
+// חייב להיות לפני החיפוש לפי ID כי אחרת הוא יחשוב ש-"search" הוא ID
+router.get('/search', validateRoomSearch, searchRooms);
 
 router.route("/:id")
-    .get(getRoomById)
-    .put(updateRoom)
-    .delete(deleteRoom);
-
-router.get('/search', validateRoomSearch, searchRooms);
+  .get(getRoomById)
+  .put(updateRoom)
+  .delete(deleteRoom);
 
 // נתיב כללי (POST ליצירה, GET לרשימה)
 router.route("/")
